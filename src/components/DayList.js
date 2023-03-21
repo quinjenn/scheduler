c17823e727de61b735c6ddcebc9b87c4f1b57d40
quinjenn <jquintal_@hotmail.com>
2023-03-21 15:34:03 -0700

    add state to app

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -10,7 +10,7 @@ export default function DayList(props) {
         name={day.name}
         spots={day.spots}
         selected={day.selected}
-        setDay={day.setDay} />);
+        setDay={props.setDay} />);
   });
 
   return (
