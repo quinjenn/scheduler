cd0f5118ed68c7eeb209aa5c763e920bc5a04a3b
quinjenn <jquintal_@hotmail.com>
2023-03-21 13:18:21 -0700

    intial commit w/ buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,43 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
+// AFTER REFRACTOR
 export default function Button(props) {
-   return <></>;
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
+
+   return (
+      <button
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   );
 }
+
+// BEFORE REFRACTOR 
+// export default function Button(props) {
+//    let buttonClass = "button";
+
+//    if (props.confirm) {
+//       buttonClass += " button--confirm";
+//    }
+//    if (props.danger) {
+//       buttonClass += " button--danger";
+//    }
+//    return (
+//       <button
+//          className={buttonClass}
+//          onClick={props.onClick}
+//          disabled={props.disabled}
+//       >
+//          {props.children}
+//       </button>
+//    );
+// }
